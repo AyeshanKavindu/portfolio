a9f905015b11d96f198f08b386a67c340ca43bbb
ayeshan <ayeshankavindu10@gmail.com>
2025-06-15 12:36:43 +0200

    ubdate3

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// Optional JavaScript for animations or interactivity
-console.log("Portfolio Loaded");
+document.addEventListener("DOMContentLoaded", function () {
+  new Typed(".typed-text", {
+    strings: ["Front End Developer", "WordPress Developer", "Web Designer", "Freelancer"],
+    typeSpeed: 60,
+    backSpeed: 40,
+    backDelay: 2000,
+    loop: true,
+    showCursor: false,
+  });
+});
