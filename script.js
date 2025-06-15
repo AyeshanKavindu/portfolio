aef16ac100ae7d7c00e0ddadc03a6bd8cb4fa10b
ayeshan <ayeshankavindu10@gmail.com>
2025-06-15 12:44:02 +0200

    :update5

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   new Typed(".typed-text", {
     strings: ["Front End Developer", "WordPress Developer", "Web Designer", "Freelancer"],
     typeSpeed: 60,
-    backSpeed: 40,
-    backDelay: 2000,
+    backSpeed: 30,
+    backDelay: 1500,
     loop: true,
-    showCursor: false,
+    showCursor: false // since you already have a custom blinking cursor
   });
 });
+
